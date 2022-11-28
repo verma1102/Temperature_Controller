38f8dd866bff73c8620e6eba878eccd3918db704
vPiyush1102 <piyush.verma@squbix.com>
2022-11-28 15:38:43 +0530

    Updated Dependencies Version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [TempValue, setTempValue] = useState(14);
-  const [TempColor, setTempColor] = useState('cold');
+  const [TempColor, setTempColor] = useState("cold");
 
   const increaseTemp = () => {
     const newTemp = TempValue + 1;
     setTempValue(newTemp);
 
     if (newTemp > 17) {
-      setTempColor('hot');
+      setTempColor("hot");
     }
   };
 
@@ -18,18 +18,18 @@ const App = () => {
     const newTemp = TempValue - 1;
     setTempValue(newTemp);
     if (newTemp <= 17) {
-      setTempColor('cold');
+      setTempColor("cold");
     }
   };
 
   return (
     <>
       <h3>TEMPERATURE CONTROLLER</h3>
-      <div className='app-container'>
-        <div className='Temp-display-container'>
+      <div className="app-container">
+        <div className="Temp-display-container">
           <div className={`Temp-display ${TempColor}`}>{TempValue}°C</div>
         </div>
-        <div className='button-container'>
+        <div className="button-container">
           <button onClick={decreaseTemp}>-</button>
           <button onClick={increaseTemp}>+</button>
         </div>
